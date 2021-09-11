ff0f6d5302c775bd66cdb2c54181ec1d6d61cbf5
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-09-11 09:39:45 -0700

    skills icons

--- my-portfolio/src/Views/Homepage/Homepage.jsx
+++ my-portfolio/src/Views/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Skills from '../Skills/Skills'
 
 const Homepage = () => {
   return (
     <div className=' justify-center place-content-center block my-20'>
       <h2 className='mb-6 animate-pulse text-6xl'> Nash Anderson</h2>
       <h3 className='text-2xl'>Full-Stack Web Developer</h3>
+      <Skills/>
       </div>
   )
 }
--- my-portfolio/src/Views/Skills/Skills.jsx
+++ my-portfolio/src/Views/Skills/Skills.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
-    <div>
-      skills
+    <div className="flex items-center justify-center ">
+      <div className='grid grid-cols-7 my-8 gap-6'>
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-plain.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-plain.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg" />
+      <img className='w-14 h-14' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
