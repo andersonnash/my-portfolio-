6089ef62aa4a49b08f9439d526a052f0ad6aad5c
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-09-11 08:05:28 -0700

    posted resume

--- my-portfolio/src/Views/Resume/Resume.jsx
+++ my-portfolio/src/Views/Resume/Resume.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div>
-      resume
+    <div className='flex justify-center shadow-2xl'>
+      <img src='https://i.imgur.com/EQh7l6e.jpg'/>
     </div>
   )
 }
