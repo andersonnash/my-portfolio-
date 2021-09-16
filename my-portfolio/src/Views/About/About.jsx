bef87715c7588c8c144202e3ea469e11d16a4fba
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-09-16 11:40:08 -0600

    downloadable resume

---
+++ my-portfolio/src/Views/About/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>
+      Innovative and technically astute Software Engineer well-versed in
+      analyzing user needs and developing software to precisely meet diverse
+      needs. Demonstrates mastery of multiple programming languages and
+      proactively keeps up with industry trends. Driven to wear many programming
+      hats with expertise in creating everything from new applications to new
+      operating systems.
+      </h3>
+    </div>
+  );
+};
+
+export default About;
--- my-portfolio/src/Views/Homepage/Homepage.jsx
+++ my-portfolio/src/Views/Homepage/Homepage.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Skills from '../Skills/Skills'
+import React from "react";
+import Skills from "../Skills/Skills";
+import About from "../About/About";
 
 const Homepage = () => {
   return (
-    <div className=' justify-center place-content-center block my-20'>
-      <h2 className='mb-6 animate-pulse text-6xl'> Nash Anderson</h2>
-      <h3 className='text-2xl'>Full-Stack Web Developer</h3>
-      <Skills/>
-      </div>
-  )
-}
+    <div className=" justify-center place-content-center block my-20">
+      <h3 className='text-8xl font-bold transition-opacity'>Hello! I am,</h3>
+      <h2 className="mb-6 animate-pulse text-8xl"> Nash Anderson</h2>
+      <h3 className="text-8xl">Full-Stack Web Developer</h3>
+      <hr className='mt-20'/>
+      <Skills />
+      <hr className='mb-20'/>
+      <About />
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
